7f75e7e3b6c62e3f26afee08dc1b03899bd4a898
Arman <armanfathabadi99@gmail.com>
2024-05-30 23:08:59 -0400

    Revert "Changes on the about us section"
    This reverts commit 69e56b21a606750bb4ba430a748dee65aa59829c.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,23 @@
-// tailwind.config.js
 module.exports = {
+  mode: "jit",
+  content: ["./src/**/**/*.{js,ts,jsx,tsx,html,mdx}", "./src/**/*.{js,ts,jsx,tsx,html,mdx}"],
+  darkMode: "class",
   theme: {
+    screens: { md: { max: "1050px" }, sm: { max: "550px" } },
     extend: {
       colors: {
-        "blue_gray-300": "#CBD5E0", // Example color, adjust as needed
+        gray: { 400: "#c8c8c8", 900: "#1b1b1b", "900_01": "#191919", "800_7f": "#4848487f", "900_02": "#1a1a1a" },
+        white: { A700_01: "#ffffff", A700: "#fcfdff", A700_99: "#ffffff99" },
+        black: { 900: "#000000" },
+        blue_gray: { 300: "#86beaa", 400: "#8d8d8d", 900: "#2b2b2b", "400_01": "#858a99", "300_01": "#85bda9" },
       },
-      spacing: {
-        "83px": "83px",
-        "39px": "39px",
-        "78px": "78px",
-        "26px": "26px",
+      boxShadow: {
+        xs: "0px 0px 60px 30px #00000007",
+        sm: "0px 0px 14px 0px #0000001e",
+        md: "0px 0px 6px 0px #00000019",
       },
-      fontSize: {
-        xl: ["2rem", "2.5rem"], // Example size, adjust as needed
-      },
-      zIndex: {
-        1: "1",
-      },
+      fontFamily: { poppins: "Poppins", manrope: "Manrope", roboto: "Roboto" },
     },
   },
+  plugins: [require("@tailwindcss/forms")],
 };
